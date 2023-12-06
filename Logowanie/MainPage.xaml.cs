2cc10a8e248b6cb51d762d6b3996d416506089e1
Bart1251 <bartlomiej.wiecek@vp.pl>
2023-12-06 11:31:43 +0100

    error fixed2

--- Logowanie/MainPage.xaml.cs
+++ Logowanie/MainPage.xaml.cs
@@ -27,7 +27,7 @@ namespace Logowanie
             InitializeComponent();
         }
 
-        public void LoginClick(object sender, EventArgs e)
+        public void LoginClick(object sender, EventArgs args)
         {
             if (string.IsNullOrEmpty(Login.Text) || string.IsNullOrEmpty(Password.Text))
                 return;
